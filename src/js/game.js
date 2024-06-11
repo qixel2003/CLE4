9dc2dd7336c6cc4b6373b1cc077563d955caab3b
qixel2003 <66803720+qixel2003@users.noreply.github.com>
2024-06-11 11:06:54 +0200

    scene change setup

--- src/js/game.js
+++ src/js/game.js
@@ -1,27 +1,40 @@
 import '../css/style.css'
 import { Actor, Engine, Vector, DisplayMode } from "excalibur"
 import { Resources, ResourceLoader } from './resources.js'
+import { Level } from './room1.js'
+// import { Level2 } from './room2.js'
+// import { GameOver } from './game_over.js'
+// import { LevelClear } from './levelclear.js'
+// import { Intro } from './intro.js'
+
+const options = {
+    width: 1200, height: 720, maxFps:60,
+    backgroundColor: Color.Black,
+    displayMode: DisplayMode.FitScreen,
+    physics: {
+        solver: SolverStrategy.Realistic,
+        gravity: new Vector(0, 800),
+    }
+}
 
 export class Game extends Engine {
 
     constructor() {
-        super({ 
-            width: 1280,
-            height: 720,
-            maxFps: 60,
-            displayMode: DisplayMode.FitScreen
-         })
+        super(options)
+        this.showDebug(true)
         this.start(ResourceLoader).then(() => this.startGame())
     }
 
     startGame() {
-        console.log("start de game!")
-        const fish = new Actor()
-        fish.graphics.use(Resources.Fish.toSprite())
-        fish.pos = new Vector(400, 300)
-        fish.vel = new Vector(-10,0)
-        this.add(fish)
+        // this.add('intro', new Intro())
+        this.add('room1', new Room1())
+        // this.add('room2', new Room2())
+        // this.add('gameover', new GameOver())
+        // this.add('levelclear', new LevelClear())
+        // this.goToScene('intro')
+        this.goToScene('room1')
     }
+
 }
 
 new Game()
---
+++ src/js/room1.js
@@ -0,0 +1,33 @@
+import '../css/style.css';
+import { Actor, Engine, Vector, Color, Label, FontUnit, Font, BoundingBox, SolverStrategy, Scene } from "excalibur";
+import { Resources, ResourceLoader } from './resources.js';
+
+export class Room1 extends Scene {
+    onInitialize(engine) {
+        this.startLevel();
+    }
+
+    onActivate(ctx) {
+        console.log("reset the level");
+        this.resetGame();
+        this.startLevel(); // Re-initialize the level
+    }
+
+    startLevel() {
+        
+    }
+
+    onPreUpdate(engine) {
+        // Add any necessary updates per frame here
+    }
+
+    
+
+    resetGame() {
+        // Clear all existing actors
+        this.actors.forEach(actor => {
+            this.remove(actor);
+        });
+    }
+}
+new Level
