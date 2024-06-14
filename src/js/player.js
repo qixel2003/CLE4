3dfd834154dc57ceacda725c44d50b0e11d480ed
qixel2003 <66803720+qixel2003@users.noreply.github.com>
2024-06-14 11:21:20 +0200

    player sprites and basic enemy behaviour

--- src/js/player.js
+++ src/js/player.js
@@ -1,4 +1,4 @@
-import { Actor, Color, Vector, Input, CollisionType } from "excalibur";
+import { Actor, Color, Vector, Input } from "excalibur";
 import { Resources, ResourceLoader } from './resources.js'
 
 export class Player extends Actor {
